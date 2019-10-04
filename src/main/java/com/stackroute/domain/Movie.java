72848c5dfe9224e58b18b9a7603f9593638a3e87
PoorviBhat <kpoorvibhat@gmail.com>
2019-10-04 15:46:43 +0530

    task 12

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -18,6 +19,7 @@ public class Movie {
     @Id
     private Integer id;
     private String title;
+    @Type(type = "org.hibernate.type.NumericBooleanType")
     private boolean adult;
     private String overview;
 
